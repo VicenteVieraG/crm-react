6b93d6da3a93c07f867eae3dc1fb3aa359d6016a
Vicente Viera <plantedcoot706@gmail.com>
2022-06-03 08:50:26 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './layout/Layout'
+import Inicio from './paginas/Inicio'
+import NuevoCliente from './paginas/NuevoCliente'
+import EditarCliente from './paginas/EditarCliente'
+
+function App() {
+    return (
+        <div>
+            {/*BrowserRouter indica que se van a estar usando rutas
+             y es lo que las esta verificando.
+             
+             Dentro de Routes Van a estar las rutas
+             
+             Las Route con etiqueta de apertura y cierre indican que contienen rutas anidadas
+             y las Route con solo cierre indican una ruta individual
+             
+             El atributo index indica cual es el componente default que se va a cargar en el
+             outlet de la ruta padre
+             
+             Para anidar mas componentes con nuevas rutas se le agregan a las rutas hijas 
+             la propiedad path y eso se va a cargar en el outlet del componente padre.
+
+             Escribiendo :x (x=cualquier nombre) despues de un path indica que el path puede ser
+             varible*/}
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/clientes" element={<Layout/>}>
+                        <Route index element={<Inicio/>}/>
+                        <Route path="nuevo" element={<NuevoCliente/>}/>
+                        <Route path="editar/:id" element={<EditarCliente/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+    }
+
+export default App
