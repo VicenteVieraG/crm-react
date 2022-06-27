427a9fa6d88382fefee4c56d54e560a5682668be
Vicente Viera <plantedcoot706@gmail.com>
2022-06-27 15:43:41 -0500

    More styles added to the nav bar, fixed sintaxis and performance issues and fixed documentation

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom'
+import {Outlet, Link} from 'react-router-dom'
+//Link es el componente con el que modificas la direccion de la barra de navegacion del navegador. 
+//En el atributo "to" selecciones la direccion que dentra la barra de navegacion.
 
 const Layout = () => {
 	return (
 		<div className="md:flex md:min-h-screen">
+			{/*Nav bar */}
 			<div className="md:w-1/4 bg-blue-900 px-5 py-10">
 				<h2 className="text-4xl font-black text-center text-white">CRK - Clientes</h2>
 				<nav className="mt-10">
-					<a 
-						className="text-white text-2xl block mt-2 hover:text-blue-300"
-						href="/clientes"
+					<Link 
+						className="transition delay-75 duration-200 text-white text-2xl block mt-2 hover:text-blue-300"
+						to="/clientes"
 						>Clientes
-					</a>
-					<a href="/clientes/nuevo">Nuevo Cliente</a>
-				</nav>
-				1
+					</Link>
+					<Link 
+						className="transition delay-75 duration-200 text-white text-2xl block mt-2 hover:text-blue-300"
+						to="/clientes/nuevo"
+						>Nuevo Cliente
+					</Link>
+				</nav>	
 			</div>
-			<div className="md:w-3/4 bg-blue-900 px-5 py-10">				
+
+			<div className="md:w-3/4">				
 				<Outlet/>
 			</div>
 			
