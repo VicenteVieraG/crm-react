52fc460ca9bcc96ae3bb9ce82e53107ea74316cd
Vicente Viera <plantedcoot706@gmail.com>
2022-06-28 16:31:55 -0500

    form added
    first version of the form, still not working

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import {Outlet, Link} from 'react-router-dom'
+import {Outlet, Link, useLocation} from 'react-router-dom'
 //Link es el componente con el que modificas la direccion de la barra de navegacion del navegador. 
 //En el atributo "to" selecciones la direccion que dentra la barra de navegacion.
 
 const Layout = () => {
+	const location = useLocation();
+	const URLActual = location.pathname;
+
 	return (
 		<div className="md:flex md:min-h-screen">
 			{/*Nav bar */}
@@ -11,19 +14,19 @@ const Layout = () => {
 				<h2 className="text-4xl font-black text-center text-white">CRK - Clientes</h2>
 				<nav className="mt-10">
 					<Link 
-						className="transition delay-75 duration-200 text-white text-2xl block mt-2 hover:text-blue-300"
+						className={`${URLActual === "/clientes" ? "text-blue-300" : "text-white"} transition delay-75 duration-200 text-2xl block mt-2 hover:text-blue-300`}
 						to="/clientes"
 						>Clientes
 					</Link>
 					<Link 
-						className="transition delay-75 duration-200 text-white text-2xl block mt-2 hover:text-blue-300"
+						className={`${URLActual === "/clientes/nuevo" ? "text-blue-300" : "text-white"} transition delay-75 duration-200 text-2xl block mt-2 hover:text-blue-300`}
 						to="/clientes/nuevo"
 						>Nuevo Cliente
 					</Link>
 				</nav>	
 			</div>
 
-			<div className="md:w-3/4">				
+			<div className="md:w-3/4 p-10">				
 				<Outlet/>
 			</div>
 			
