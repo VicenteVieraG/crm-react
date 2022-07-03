a05dde79d29ac1b001270685aee037d9f00ad411
Vicente Viera <plantedcoot706@gmail.com>
2022-07-03 17:04:46 -0500

    Interactive chart implemented. Chart desing added. feth from the api implemented.

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Cliente = ({cliente}) => {
+    const {nombre, empresa, email, telefono, notas, id} = cliente
+
+    return (
+        <tr className="transition ease-in-out duration-200 border-b hover:bg-gray-50">
+            <td className="p-3">{nombre}</td>
+            <td className="p-3">
+                <p><span className="text-gray-800 uppercase font-bold">E-mail:</span>{email}</p>
+                <p><span className="text-gray-800 uppercase font-bold">Tel:</span>{telefono}</p>
+            </td>
+            <td className="p-3">{empresa}</td>
+            <td className="p-3">
+                <button
+                    type="button"
+                    className="transition ease-in-out duration-200 bg-yellow-500 text-white font-bold uppercase text-xs block w-full p-2 hover:bg-yellow-600"
+                    >Ver
+                </button>
+                <button
+                    type="button"
+                    className="transition ease-in-out duration-200 bg-blue-600 text-white font-bold uppercase text-xs block w-full p-2 mt-3 hover:bg-blue-700"
+                    >Editar
+                </button>
+                <button
+                    type="button"
+                    className="transition ease-in-out duration-200 bg-red-600 text-white font-bold uppercase text-xs block w-full p-2 mt-3 hover:bg-red-700"
+                    >Eliminar
+                </button>
+            </td>
+        </tr>
+    )
+}
+
+export default Cliente
--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
-import {useEffect} from 'react'
- 
+import Cliente from '../components/Cliente';
+import {useEffect, useState} from 'react'
+
 const Inicio = () => {
-  return (
-    <div>
-        Inicio
-    </div>
-  )
+	const [clientes, setClientes] = useState([]);
+
+	useEffect(() => {
+		const obtenerCientesAPI = async () => {
+			try {
+				const url = "http://localhost:4000/clientes";
+				const respuesta = await fetch(url);
+				const resultado = await respuesta.json();
+
+				setClientes(resultado);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		obtenerCientesAPI();
+	},[]);
+
+	return (
+		<div>
+			<h1 className="font-black text-4xl text-blue-900">Cliente</h1>
+        	<p className="mt-3">Administra tus clientes</p>
+
+			<table className="w-full mt-5 table-auto shadow bg-white">
+				<thead className="bg-blue-800 text-white">
+					<tr>
+						<th className="p-2">Nombre</th>
+						<th className="p-2">Contacto</th>
+						<th className="p-2">Empresa</th>
+						<th className="p-2">Acciones</th>
+					</tr>
+				</thead>
+					{clientes.map(cliente => (<Cliente key={cliente.id} cliente={cliente}/>))}
+				<tbody>
+
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default Inicio
